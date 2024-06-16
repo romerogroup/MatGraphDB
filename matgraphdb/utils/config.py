406291652a60320dde13addf3845cff45000d688
Logan L Lang <lllang@trcis001.hpc.wvu.edu>
2024-06-16 13:53:49 -0400

    added method to get number of cores through environment variables

--- matgraphdb/utils/config.py
+++ matgraphdb/utils/config.py
@@ -8,6 +8,39 @@ large_width = 400
 precision=3
 np.set_printoptions(linewidth=large_width,precision=precision)
 
+
+def get_cpus_per_node():
+    cpus_per_node = os.getenv('SLURM_JOB_CPUS_PER_NODE')
+    if cpus_per_node is None:
+        cpus_per_node = 1
+    elif '(x' in cpus_per_node:
+        cpu_per_node, num_nodes= cpus_per_node.strip(')').split('(x')
+        cpus_node_list = [int(cpu_per_node) for _ in range(int(num_nodes))]
+    else:
+        cpus_node_list = [int(x) for x in cpus_per_node.split(',')]
+    return cpus_node_list
+
+def get_num_tasks():
+    num_tasks = os.getenv('SLURM_NTASKS')
+    if num_tasks:
+        num_tasks = int(num_tasks)
+    return num_tasks
+
+def get_num_nodes():
+    num_nodes = int(os.getenv('SLURM_JOB_NUM_NODES'))
+    return num_nodes
+
+def get_total_cores(cpus_per_node):
+    return sum(cpus_per_node)
+
+def get_num_cores(n_cores=None):
+    if n_cores:
+        return n_cores
+    else:
+        cpus_per_node = get_cpus_per_node()
+        return get_total_cores(cpus_per_node)
+
+
 # Important directory paths
 FILE = Path(__file__).resolve()
 PKG_DIR = str(FILE.parents[1])  # poly_graph_lib
@@ -23,7 +56,7 @@ PRIVATE_CONFIG_FILE=os.path.join(ROOT,'private_config.yml')
 with open(CONFIG_FILE, 'r') as f:
     CONFIG = yaml.safe_load(f)
 
-N_CORES=CONFIG['N_CORES']
+N_CORES=get_num_cores(n_cores=CONFIG['N_CORES'])
 
 # Neo4j variables
 USER=CONFIG['USER']
@@ -46,7 +79,8 @@ DB_DIR=os.path.join(MP_DIR,'json_database')
 DATASETS_DIR=os.path.join(MP_DIR,'datasets')
 
 GRAPH_DIR=os.path.join(MP_DIR,'graph_database')
-MAIN_GRAPH_DIR=os.path.join(GRAPH_DIR,'main')
+NEO4J_GRAPH_DIR=os.path.join(GRAPH_DIR,'neo4j')
+MAIN_GRAPH_DIR=os.path.join(NEO4J_GRAPH_DIR,'main')
 ENCODING_DIR=os.path.join(MP_DIR,'encodings')
 SIMILARITY_DIR=os.path.join(MP_DIR,'similarities')
 NODE_DIR=os.path.join(GRAPH_DIR,'nodes')
@@ -56,5 +90,7 @@ GLOBAL_PROP_FILE=os.path.join(MP_DIR,'global_properties.json')
 
 NEO4J_DESKTOP_DIR=CONFIG['NEO4J_DESKTOP_DIR']
 DBMSS_DIR = os.path.join(NEO4J_DESKTOP_DIR,'relate-data','dbmss')
+
+
 
 
