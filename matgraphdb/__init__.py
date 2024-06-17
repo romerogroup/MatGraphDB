f247012e60e439854f4278a3fb228566f1978371
lllangWV <lllangWV@gmail.com>
2024-06-17 10:32:36 -0400

    Added DBManager, Neo4jGDSManager,Neo4jGraphDatabase,GraphGenerator to top import

--- matgraphdb/__init__.py
+++ matgraphdb/__init__.py
@@ -1,3 +1,5 @@
 __version__='1.0.0'
 
 from matgraphdb.data.manager import DBManager
+from matgraphdb.graph.graph_database import Neo4jGDSManager,Neo4jGraphDatabase
+from matgraphdb.graph.graph_generator import GraphGenerator
