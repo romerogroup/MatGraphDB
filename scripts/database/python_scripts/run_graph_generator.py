b834b5e51539aa5d871cf2fcdf0567110e9d2432
Logan L Lang <lllang@trcis001.hpc.wvu.edu>
2024-06-18 11:52:40 -0400

    Updated out of package scripts

--- scripts/database/python_scripts/run_graph_generator.py
+++ scripts/database/python_scripts/run_graph_generator.py
@@ -2,29 +2,29 @@ from matgraphdb.graph.graph_generator import GraphGenerator
 
 def main():
     generator=GraphGenerator(from_scratch=False,skip_main_init=False)
+    main_graph_dir = generator.main_graph_dir
+    generator.screen_graph_database(main_graph_dir,'nelements-2-2',nelements=(2,2), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-3-3',nelements=(3,3), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-4-4',nelements=(4,4), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-5-5',nelements=(5,5), from_scratch=True)
 
-    generator.screen_graph_database('nelements-2-2',nelements=(2,2), from_scratch=True)
-    generator.screen_graph_database('nelements-3-3',nelements=(3,3), from_scratch=True)
-    generator.screen_graph_database('nelements-4-4',nelements=(4,4), from_scratch=True)
-    generator.screen_graph_database('nelements-5-5',nelements=(5,5), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-2-3',nelements=(1,3), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-2-4',nelements=(1,4), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-2-5',nelements=(1,5), from_scratch=True)
 
-    generator.screen_graph_database('nelements-2-3',nelements=(1,3), from_scratch=True)
-    generator.screen_graph_database('nelements-2-4',nelements=(1,4), from_scratch=True)
-    generator.screen_graph_database('nelements-2-5',nelements=(1,5), from_scratch=True)
-
-    generator.screen_graph_database('nelements-3-4',nelements=(3,4), from_scratch=True)
-    generator.screen_graph_database('nelements-3-5',nelements=(3,5), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-3-4',nelements=(3,4), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-3-5',nelements=(3,5), from_scratch=True)
     
-    generator.screen_graph_database('nelements-4-5',nelements=(4,5), from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'nelements-4-5',nelements=(4,5), from_scratch=True)
 
-    generator.screen_graph_database('spg-145',space_groups=[145], from_scratch=True)
-    generator.screen_graph_database('spg-145-196',space_groups=[145,196], from_scratch=True)
-    generator.screen_graph_database('spg-no-145',space_groups=[145], from_scratch=True, include=False)
-    generator.screen_graph_database('spg-no-196',space_groups=[196], from_scratch=True, include=False)
+    generator.screen_graph_database(main_graph_dir,'spg-145',space_groups=[145], from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'spg-145-196',space_groups=[145,196], from_scratch=True)
+    generator.screen_graph_database(main_graph_dir,'spg-no-145',space_groups=[145], from_scratch=True, include=False)
+    generator.screen_graph_database(main_graph_dir,'spg-no-196',space_groups=[196], from_scratch=True, include=False)
 
-    generator.screen_graph_database('elements-no-Ti',elements=["Ti"], from_scratch=True, include=False)
-    generator.screen_graph_database('elements-no-Fe',elements=["Fe"], from_scratch=True, include=False)
-    generator.screen_graph_database('elements-no-Ti-Fe',elements=["Ti","Fe"], from_scratch=True, include=False)
+    generator.screen_graph_database(main_graph_dir,'elements-no-Ti',elements=["Ti"], from_scratch=True, include=False)
+    generator.screen_graph_database(main_graph_dir,'elements-no-Fe',elements=["Fe"], from_scratch=True, include=False)
+    generator.screen_graph_database(main_graph_dir,'elements-no-Ti-Fe',elements=["Ti","Fe"], from_scratch=True, include=False)
 
 if __name__ == '__main__':
     main()
--- scripts/database/python_scripts/run_write_graphml.py
+++ scripts/database/python_scripts/run_write_graphml.py
@@ -1,8 +1,11 @@
+import os
 from matgraphdb.graph.graph_generator import GraphGenerator
 
 def main():
-    graph=GraphGenerator()
-    graph.write_graphml(graph_dirname='nelements-2-2')
+    generator=GraphGenerator()
+    main_graph_dir = generator.main_graph_dir
+    graph_dir= os.path.join(main_graph_dir,'sub_graphs','nelements-2-2')
+    generator.write_graphml(graph_dirname=graph_dir)
 
 if __name__ == '__main__':
     main()
