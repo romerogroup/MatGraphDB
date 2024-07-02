6ffea2e5de287b1f73a243cf41730afe8d04b922
lllangWV <lllangWV@gmail.com>
2024-07-02 16:42:50 -0400

    Added profiling script

---
+++ scripts/testing_scripts/python_scripts/profile_script.py
@@ -0,0 +1,31 @@
+import os
+
+import cProfile
+import pstats
+
+
+def main():
+    from matgraphdb.graph.node_types import NodeTypes
+    nodes=NodeTypes()
+    names=nodes.get_chemenv_element_nodes()
+    # print(names[:10])
+
+
+if __name__ == "__main__":
+    from matgraphdb.utils import ROOT
+    profiler = cProfile.Profile()
+    profiler.enable()
+    print(ROOT)
+    # Run the function you want to profile
+    main()
+    
+    profiler.disable()
+    
+    # Save the stats to a file
+    stats = pstats.Stats(profiler)
+    stats.sort_stats('tottime')
+    stats.dump_stats(os.path.join(ROOT,'logs','profile_results.prof'))  # Save to .prof file
+    with open(os.path.join(ROOT,'logs','profile_results.txt'), 'w') as f:
+        stats = pstats.Stats(os.path.join(ROOT,'logs','profile_results.prof'), stream=f)
+        stats.sort_stats('tottime')
+        stats.print_stats()
