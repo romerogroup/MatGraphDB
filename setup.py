83f45479529e492cc06d18a258d9c358e8fa9739
lllangWV <lllangWV@gmail.com>
2024-09-30 14:09:48 -0400

    Some changes involving packages

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-import re
-from setuptools import find_packages
-from typing import List
-from distutils.core import setup
-from pathlib import Path
+from setuptools import setup
 
-import pkg_resources as pkg
-
-# Settings
-FILE = Path(__file__).resolve()
-PARENT = FILE.parent  # root directory
-README = (PARENT / 'README.md').read_text(encoding='utf-8')
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
-
-
-
-# def get_version():
-#     file = PARENT / 'poly_graphs_lib/__init__.py'
-#     version= re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', file.read_text(encoding='utf-8'), re.M)[1]
-#     print(version)
-#     return version
-
-
-setup(
-name='matgraphdb',
-version="1.0.0",
-author='Logan Lang',
-author_email='lllang@mix.wvu.edu',
-packages=find_packages(),
-install_requires=REQUIREMENTS
-        )
+setup()
