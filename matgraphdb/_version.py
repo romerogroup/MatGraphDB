11e98d26341dcb08e48f7d717f5e0243a19237d2
lllangWV <lllangWV@gmail.com>
2024-10-01 00:27:46 -0400

    removed large png file

--- matgraphdb/_version.py
+++ matgraphdb/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev257+gf3d2c4e.d20241001'
-__version_tuple__ = version_tuple = (0, 1, 'dev257', 'gf3d2c4e.d20241001')
+__version__ = version = '0.0.1.dev0+gdea13fa.d20241001'
+__version_tuple__ = version_tuple = (0, 0, 1, 'dev0', 'gdea13fa.d20241001')
