52b87a62edae718c50c9a4e467ea790dbedcc3d9
lllangWV <lllangWV@gmail.com>
2024-09-30 20:48:57 -0400

    changes to the tmp file

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -1,19 +1,26 @@
-from matgraphdb import DBManager
+import torch
 
 
-if __name__=='__main__':
-    db=DBManager()
-    db.fix_chargemol_name()
+print(torch.__version__)
+print(torch.cuda.is_available())
 
 
-# with open(os.path.join(DB_DIR,'mp-1000.json')) as f:
-#     data = json.load(f)
+# from matgraphdb import DBManager
 
-# print(data['chargemol_bonding_connections'])
-# print(data['chargemol_bonding_orders'])
 
-# data['chargemol_bonding_orders']=data['chargemol_bonding_connections']
-# data['chargemol_bonding_connections']=data['chargemol_bonding_orders']
+# if __name__=='__main__':
+#     db=DBManager()
+#     db.fix_chargemol_name()
 
-# with open(os.path.join(DB_DIR,'mp-1000.json'),'w') as f:
-#     json.dump(data, f, indent=4)
+
+# # with open(os.path.join(DB_DIR,'mp-1000.json')) as f:
+# #     data = json.load(f)
+
+# # print(data['chargemol_bonding_connections'])
+# # print(data['chargemol_bonding_orders'])
+
+# # data['chargemol_bonding_orders']=data['chargemol_bonding_connections']
+# # data['chargemol_bonding_connections']=data['chargemol_bonding_orders']
+
+# # with open(os.path.join(DB_DIR,'mp-1000.json'),'w') as f:
+# #     json.dump(data, f, indent=4)
