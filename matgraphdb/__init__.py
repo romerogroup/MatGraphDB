a27eb93fc7d8771f9808646efd4be99c8bab4d58
lllangWV <lllangWV@gmail.com>
2024-10-01 14:36:25 -0400

    switched back to setuptools scm

--- matgraphdb/__init__.py
+++ matgraphdb/__init__.py
@@ -1,10 +1,6 @@
-from matgraphdb._version import __version__
-
 # from matgraphdb.data.manager import DBManager
 # from matgraphdb.graph.neo4j_gds_manager import Neo4jGDSManager
 # from matgraphdb.graph.neo4j_manager import Neo4jManager
 # from matgraphdb.graph.graph_generator import GraphGenerator
 
 from matgraphdb.core import MatGraphDB
-from . import _version
-__version__ = _version.get_versions()['version']
