21ed9cc8c8dbb17cf0133e79920a8f93d8149205
lllangWV <lllangWV@gmail.com>
2024-10-03 09:53:01 -0400

    Updated to have matgraphdb to have __version__ variable

--- matgraphdb/__init__.py
+++ matgraphdb/__init__.py
@@ -1,6 +1,2 @@
-# from matgraphdb.data.manager import DBManager
-# from matgraphdb.graph.neo4j_gds_manager import Neo4jGDSManager
-# from matgraphdb.graph.neo4j_manager import Neo4jManager
-# from matgraphdb.graph.graph_generator import GraphGenerator
-
+from matgraphdb._version import __version__
 from matgraphdb.core import MatGraphDB
