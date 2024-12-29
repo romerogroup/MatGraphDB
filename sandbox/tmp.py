ff2a0dd955a00aeea01923d1b0a64fe12fffd898
lllangWV <lllangWV@gmail.com>
2024-12-29 12:19:21 -0500

    Added temporary script to test data handling

--- sandbox/tmp.py
+++ sandbox/tmp.py
@@ -6,22 +6,58 @@ import re
 import logging
 import yaml
 import logging.config
+import pickle
 
-from matgraphdb import MatGraphDB, config, logging_config
+import pyarrow as pa
+from pymatgen.core import Structure
+from parquetdb import ParquetDB
+from pyarrow import compute as pc
+
+from matgraphdb import MatGraphDB, config
+from matgraphdb.utils.parquet_tools import write_schema_summary
+
+import time
 
 
-def magicmethod(clazz, method):
-    if method not in clazz.__dict__:  # Not defined in clazz : inherited
-        return 'inherited'
-    elif hasattr(super(clazz), method):  # Present in parent : overloaded
-        return 'overloaded'
-    else:  # Not present in parent : newly defined
-        return 'newly defined'
-    
-    
-# import inspect
-# for cls in MyClass.mro():
-#     if 'my_method' in cls.__dict__:
-#         print(f"my_method is defined in {cls}")
-#         print(inspect.getsource(cls.my_method))
-#         break
+# config.logging_config.loggers.matgraphdb.level = 'DEBUG'
+# config.logging_config.loggers.parquetdb.level = 'DEBUG'
+# config.apply()
+
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+os.makedirs(os.path.join(config.data_dir, 'materials_schema'), exist_ok=True)
+
+materials_db = ParquetDB(db_path=os.path.join(config.data_dir, 'materials'))
+table = materials_db.read(columns=['core.atomic_numbers'])
+df=table.to_pandas()
+# print(df.head())
+
+# start_time = time.time()
+# struct_df=df['structure'].apply(Structure.from_dict)
+# end_time = time.time()
+# print(f"Time taken: {end_time - start_time:.2f} seconds")
+
+
+# matgraphdb = MatGraphDB(main_dir=os.path.join(config.data_dir, 'MatGraphDB'))
+
+# table = matgraphdb.matdb.read(columns=['core.frac_coords'])
+# print(table['core.frac_coords'].type)
+
+# mgo_structure = Structure(
+#     lattice=[[0, 2.13, 2.13], [2.13, 0, 2.13], [2.13, 2.13, 0]],
+#     species=["Mg", "O"],
+#     coords=[[0, 0, 0], [0.5, 0.5, 0.5]],
+# )
+
+# mgo_structure.add_site_property('magmom', [2.0, 2.0])
+
+# properties=dict(
+#     electronic_structure=dict(
+#         band_gap=1.0
+#     )
+# )
+# matgraphdb.matdb.add(structure=mgo_structure, properties=properties, verbose=4)
+
+
+
+
